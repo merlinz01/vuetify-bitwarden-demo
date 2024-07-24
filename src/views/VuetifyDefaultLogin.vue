9e22b0695139f29d30b9a85519da2f97347e20d8
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2024-07-24 09:11:57 -0400

    add fix for placeholders

--- src/views/VuetifyDefaultLogin.vue
+++ src/views/VuetifyDefaultLogin.vue
@@ -4,8 +4,10 @@
     <v-form>
       <v-card-title class="text-center"> Log in to the Widget </v-card-title>
       <v-card-text class="d-flex flex-column ga-2 ma-2">
-        <v-text-field type="email" prepend-inner-icon="mdi-account" label="Email" hide-details />
-        <v-text-field type="password" prepend-inner-icon="mdi-lock" label="Password" hide-details />
+        <v-text-field type="email" prepend-inner-icon="mdi-account" label="Email" hide-details
+          placeholder="text@example.com" />
+        <v-text-field type="password" prepend-inner-icon="mdi-lock" label="Password" hide-details
+          placeholder="Enter your password here" />
       </v-card-text>
       <v-card-actions>
         <v-btn type="submit" text="Log in" color="primary" variant="elevated" class="mx-auto" />
--- src/views/VuetifyOverridenLogin.vue
+++ src/views/VuetifyOverridenLogin.vue
@@ -4,8 +4,10 @@
     <v-form>
       <v-card-title class="text-center"> Log in to the Widget </v-card-title>
       <v-card-text class="d-flex flex-column ga-2 ma-2">
-        <v-text-field type="email" prepend-inner-icon="mdi-account" label="Email" hide-details />
-        <v-text-field type="password" prepend-inner-icon="mdi-lock" label="Password" hide-details />
+        <v-text-field type="email" prepend-inner-icon="mdi-account" label="Email" hide-details
+          placeholder="text@example.com" />
+        <v-text-field type="password" prepend-inner-icon="mdi-lock" label="Password" hide-details
+          placeholder="Enter your password here" />
       </v-card-text>
       <v-card-actions>
         <v-btn type="submit" text="Log in" color="primary" variant="elevated" class="mx-auto" />
@@ -18,5 +20,15 @@
 .opaqueinputs .v-text-field input {
   opacity: 1;
   transition: none;
+}
+
+.opaqueinputs .v-text-field input::placeholder {
+  opacity: 0;
+  transition: 0.15s opacity cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.opaqueinputs .v-text-field .v-field--no-label input::placeholder,
+.opaqueinputs .v-text-field .v-field--active input::placeholder {
+  opacity: 0.5;
 }
 </style>
